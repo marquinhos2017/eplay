3d39c8bb61a73d28d85e2bcdc7c76af834f85527
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2024-02-12 20:27:40 -0900

    Componentize - Parte I

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import Banner from './components/Banner'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Banner />
+  }
+])
 function App() {
   return (
     <>
@@ -9,7 +16,7 @@ function App() {
       <div className="container">
         <Header />
       </div>
-      <Banner />
+      <RouterProvider router={rotas} />
     </>
   )
 }
