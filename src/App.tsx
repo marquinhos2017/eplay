2ac2df1549a35802f644879e3bc5c23de92064cc
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2024-02-13 20:45:07 -0900

    Criando a pagina de detalhes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import Header from './components/Header'
-import Categories from './pages/Categories'
-import Home from './pages/Home'
 import { GlobalCss } from './styles'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  BrowserRouter,
-  Routes,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import Rotas from './routes'
 import Footer from './components/Footer'
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Categories from './pages/Categories'
 import Home from './pages/Home'
+import Product from './pages/Product'
 import { GlobalCss } from './styles'
 import {
   createBrowserRouter,
@@ -14,6 +15,7 @@ const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/categories" element={<Categories />} />
+    <Route path="/product/:id" element={<Product />} />
   </Routes>
 )
 
