700b8beeeda019ac6de94ff4186c9a505210a84d
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2024-04-10 23:50:33 +0100

    Criando a secao de pagamento por cartao

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,52 +1,126 @@
+import { useState } from 'react'
 import Button from '../../components/Button'
 import Card from '../../components/Card'
-import { InputGroup, Row } from './styles'
 
-const Checkout = () => (
-  <div className="container">
-    <Card title="Dados de cobranca">
-      <>
-        <Row>
-          <InputGroup>
-            <label htmlFor="fullName">Nome completo</label>
-            <input id="fullName" type="text" />
-          </InputGroup>
-          <InputGroup>
-            <label htmlFor="email">E-mail</label>
-            <input id="email" type="email" />
-          </InputGroup>
-          <InputGroup>
-            <label htmlFor="cpf">CPF</label>
-            <input id="cpf" type="text" />
-          </InputGroup>
-        </Row>
-        <h3 className="margin-top">Dados de entrega - conteudo digital</h3>
-        <Row>
-          <InputGroup>
-            <label htmlFor="deliveryEmail">E-mail</label>
-            <input type="email" id="deliveryEmail" />
-          </InputGroup>
-          <InputGroup>
-            <label htmlFor="confirmdeliveryEmail">Confirme o e-mail</label>
-            <input type="email" id="confirmdeliveryEmail" />
-          </InputGroup>
-        </Row>
-      </>
-    </Card>
-    <Card title="Pagamento">
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, et
-          atque possimus dolore ratione reprehenderit tempore cupiditate?
-          Impedit facilis tempora sapiente dolore cum sit, hic nihil, nulla
-          magnam necessitatibus perspiciatis.
-        </p>
-      </div>
-    </Card>
-    <Button title="Clique aqui para finalizar a compra" type="button">
-      Finalizar compra
-    </Button>
-  </div>
-)
+import boleto from '../../assets/images/boleto.png'
+import cartao from '../../assets/images/cartao.png'
+
+import { InputGroup, Row, TabButton } from './styles'
+
+const Checkout = () => {
+  const [payWithCard, setPayWithCard] = useState(false)
+  return (
+    <div className="container">
+      <Card title="Dados de cobranca">
+        <>
+          <Row>
+            <InputGroup>
+              <label htmlFor="fullName">Nome completo</label>
+              <input id="fullName" type="text" />
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor="email">E-mail</label>
+              <input id="email" type="email" />
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor="cpf">CPF</label>
+              <input id="cpf" type="text" />
+            </InputGroup>
+          </Row>
+          <h3 className="margin-top">Dados de entrega - conteudo digital</h3>
+          <Row>
+            <InputGroup>
+              <label htmlFor="deliveryEmail">E-mail</label>
+              <input type="email" id="deliveryEmail" />
+            </InputGroup>
+            <InputGroup>
+              <label htmlFor="confirmdeliveryEmail">Confirme o e-mail</label>
+              <input type="email" id="confirmdeliveryEmail" />
+            </InputGroup>
+          </Row>
+        </>
+      </Card>
+      <Card title="Pagamento">
+        <>
+          <TabButton
+            isActive={!payWithCard}
+            onClick={() => setPayWithCard(false)}
+          >
+            <img src={boleto} alt="boleto" />
+            Boleto bancario
+          </TabButton>
+          <TabButton
+            isActive={payWithCard}
+            onClick={() => setPayWithCard(true)}
+          >
+            <img src={cartao} alt="cartao" />
+            Cartao de credito
+          </TabButton>
+          <div className="margin-top">
+            {payWithCard ? (
+              <>
+                <Row>
+                  <InputGroup>
+                    <label htmlFor="cardOwner">Nome do titular do Cartao</label>
+                    <input id="cardOwner" />
+                  </InputGroup>
+                  <InputGroup>
+                    <label htmlFor="cpfOwner">CPF do titular do Cartao</label>
+                    <input id="cpfOwner" />
+                  </InputGroup>
+                </Row>
+                <Row marginTop="24px">
+                  <InputGroup>
+                    <label htmlFor="cardDisplayName">
+                      Nome do titular do Cartao
+                    </label>
+                    <input id="cardDisplayName" />
+                  </InputGroup>
+                  <InputGroup>
+                    <label htmlFor="cardNumber">Numero do cartao</label>
+                    <input id="cardNumber" />
+                  </InputGroup>
+                  <InputGroup maxWidth="123px">
+                    <label htmlFor="expiresMonth">Mes de expiracao</label>
+                    <input id="expiresMonth" />
+                  </InputGroup>
+                  <InputGroup maxWidth="123px">
+                    <label htmlFor="expiresYear">Ano de expiracao</label>
+                    <input id="expiresYear" />
+                  </InputGroup>
+                  <InputGroup maxWidth="48px">
+                    <label htmlFor="cardCode">CVV</label>
+                    <input id="cardCode" />
+                  </InputGroup>
+                </Row>
+                <Row marginTop="24px">
+                  <InputGroup maxWidth="150px">
+                    <label htmlFor="installments">Parcelamento</label>
+
+                    <select name="" id="">
+                      <option value="">1x de R$ 200,00</option>
+                      <option value="">2x de R$ 200,00</option>
+                      <option value="">3x de R$ 200,00</option>
+                    </select>
+                  </InputGroup>
+                </Row>
+              </>
+            ) : (
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corporis, et atque possimus dolore ratione reprehenderit tempore
+                cupiditate? Impedit facilis tempora sapiente dolore cum sit, hic
+                nihil, nulla magnam necessitatibus perspiciatis.
+              </p>
+            )}
+          </div>
+        </>
+      </Card>
+      <Button title="Clique aqui para finalizar a compra" type="button">
+        Finalizar compra
+      </Button>
+    </div>
+  )
+}
 
 export default Checkout
